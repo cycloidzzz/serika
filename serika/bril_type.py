64414be0ab028d6f4bc05fb83a97be2fcc88ebd9
cycloidzzz <cycloidz@126.com>
2023-02-03 23:15:35 +0800

    feat(serika/to_ssa.py): add ssa tranform function

--- serika/bril_type.py
+++ serika/bril_type.py
@@ -2,4 +2,5 @@ from typing import Dict, List, Tuple, Optional, Union
 
 JsonType = Union[Dict[str, 'JsonType'], List['JsonType'], str, int, float, bool,
                  None]
+BlockType = List[JsonType]
 BrilType = JsonType
